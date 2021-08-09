d0e4dd92d9ef00e81cd49dc318da49e15818bfa9
Amrendra Kumar <amarmca51@gmail.com>
2021-08-09 18:34:05 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import "./styles.css";
+import React from "react";
+import { userInfo } from "./utility.js";
+export default function App() {
+  const [state, setState] = React.useState(userInfo);
+
+  const handleChange = (e) => {
+    const updateState = Object.assign({}, state);
+    const { name, value } = e.target;
+    updateState[name].value = value;
+    setState(updateState);
+  };
+  const submitHandle = () => {
+    console.log("state", state);
+    state.fname.disable = true;
+    state.phone.disable = true;
+    state.city.disable = true;
+  };
+
+  return (
+    <div className="App">
+      <input
+        type="text"
+        value={state.fname.value}
+        name={state.fname.name}
+        onChange={handleChange}
+        disabled={state.fname.disable}
+      />
+      <input
+        type="text"
+        value={state.city.value}
+        name={state.city.name}
+        onChange={handleChange}
+        disabled={state.city.disable}
+      />
+      <input
+        type="text"
+        value={state.phone.value}
+        name={state.phone.name}
+        onChange={handleChange}
+        disabled={state.phone.disable}
+      />
+      <button type="button" onClick={submitHandle}>
+        Submit{" "}
+      </button>
+    </div>
+  );
+}
---
+++ src/utility.js
@@ -0,0 +1,5 @@
+export const userInfo = {
+  fname: { name: "fname", value: "", disable: false },
+  city: { name: "city", value: "", disable: false },
+  phone: { name: "phone", value: "", disable: false }
+};
